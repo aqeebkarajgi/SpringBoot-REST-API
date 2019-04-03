0acf6ab08f2f97b04a86de734ee9582ce50cf12e
Salitha Chathuranga <salithachathuranga94@gmail.com>
2019-04-03 23:35:35 +0530

    Change document structure of a user

--- src/main/java/com/rest/api/entity/User.java
+++ src/main/java/com/rest/api/entity/User.java
@@ -9,15 +9,25 @@ public class User {
     @Id
     private String id;
     private String name;
+    private String job;
+    private String email;
+    private String number;
+    private String company;
     private int age;
+    private String city;
 
     public User(){
 
     }
 
-    public User(String name, int age){
+    public User(String name, String job, String email, String number, int age, String company, String city){
         this.name = name;
+        this.email = email;
+        this.number = number;
+        this.job = job;
         this.age = age;
+        this.company = company;
+        this.city = city;
     }
 
     public String getId() {
@@ -35,6 +45,39 @@ public class User {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumber() { return number; }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getJob() { return job; }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getCompany() { return company; }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getCity() { return city; }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
 
     public int getAge() {
         return age;
