4d5e38595a8f974dac06a506c15cba202fd6446d
aqeebkarajgi <129368716+aqeebkarajgi@users.noreply.github.com>
2023-08-31 16:49:20 +0530

    Update UserController.java

--- src/main/java/com/rest/api/controller/UserController.java
+++ src/main/java/com/rest/api/controller/UserController.java
@@ -1,44 +1,23 @@
-package com.rest.api.controller;
-
-import com.rest.api.entity.User;
-import com.rest.api.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
-import java.util.Optional;
-
 @RestController
-@RequestMapping("/api")
-@CrossOrigin("*")
+@RequestMapping(value = "/api")
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
-    @RequestMapping(method=RequestMethod.GET, value="/users")
-    public List<User> findAllUsers() {
-        return userRepository.findAll();
+    @GetMapping(path = "/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok().body(userService.getUsers());
     }
 
-    @RequestMapping(method=RequestMethod.GET, value="/users/{id}")
-    public Optional<User> getUser(@PathVariable("id") String id) {
-        return userRepository.findById(id);
+    @GetMapping(path = "/users/{id}")
+    public ResponseEntity<User> getUserByIdPath(@PathVariable String id) {
+        return ResponseEntity.ok().body(userService.getUserById(id));
     }
 
-    @RequestMapping(method=RequestMethod.POST, value="/users/add")
-    public User addUser(@RequestBody User user) {
-        userRepository.save(user);
-        return user;
-    }
-
-    @RequestMapping(method=RequestMethod.PUT, value="/users/update")
-    public void updateUser(@RequestBody User user) {
-        userRepository.save(user);
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public void deleteUser(@PathVariable("id") String id) {
-        userRepository.deleteById(id);
+    @PostMapping(path = "/users")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        return ResponseEntity.ok(userService.saveUser(user));
     }
 
 }
